4cfd9ba86a58f88316e3e823cca408572d3a8b84
Dave Arnold <darnold@hashicorp.com>
2018-12-13 13:57:17 -0800

    changing path of sentinel policy

--- tfe_setup/main.tf
+++ tfe_setup/main.tf
@@ -2,7 +2,7 @@
 resource "tfe_sentinel_policy" "instance_type" {
   name = "instance-size-test"
   organization = "${var.organization}"
-  policy = "${file("${path.module}/../instance_type.sentinel.tpl")}"
+  policy = "${file("${path.root}/instance_type.sentinel.tpl")}"
   enforce_mode = "hard-mandatory"
 }
 
